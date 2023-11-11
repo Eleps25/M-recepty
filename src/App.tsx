fbffcbab1dfbf7883f098120e38fdf7aa4d41172
Jakub Mezulanik <mezuja@gmail.com>
2023-11-11 16:59:39 +0100

    basic routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import RecipeDetailPage from "./components/RecipeDetailPage/RecipeDetailPage";
 import RecipeList from "./components/RecipeList/RecipeList";
 
+import {Routes, Route} from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <RecipeList />
-      <RecipeDetailPage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipelist" element={<RecipeList />} />
+        <Route path="/recipelist/:id" element={<RecipeDetailPage />} />
+      </Routes>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
+);
