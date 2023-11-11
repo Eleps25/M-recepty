dac70cdf6ed6556910617b6e6390ec1f8ed0a0e5
Jakub Mezulanik <mezuja@gmail.com>
2023-11-11 15:17:05 +0100

    firebase added

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5KF9wig31JkxjXn0x-Wj9tHoJF4JoWyw",
+  authDomain: "m-recepty.firebaseapp.com",
+  projectId: "m-recepty",
+  storageBucket: "m-recepty.appspot.com",
+  messagingSenderId: "351383567286",
+  appId: "1:351383567286:web:881e678a386bbc3effdf17"
+};
+
+const app = initializeApp(firebaseConfig);
