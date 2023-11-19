059d38f9867f75dd1f386ddc90c0bb58a07b8628
Jakub Mezulanik <mezuja@gmail.com>
2023-11-19 01:07:15 +0100

    recipe - basic styles

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 
 import { IRecipeList } from "../../interfaces/RecipeList";
+import image from "../../tmpImgs/food2.jpg";
 
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./style.css";
 
 interface Props {
   recipeData: IRecipeList;
@@ -23,30 +26,44 @@ const Recipe: React.FC<Props> = (props) => {
     page,
   } = props.recipeData;
   return (
-    <div>
-      <h1>Název receptu: {title}</h1>
-      <h2>Obrázek</h2>
-      <h2>Název knihy: {book}</h2>
-      <h2>Strana: {page}</h2>
-      <h2>Typ: {mealType}</h2>
-      <h2>Počet vaření: {cookedNumber}</h2>
-      <h2>Náročnost: {difficulty}</h2>
-      <h2>Doba přípravy: {prepareTime}</h2>
-      <h2>
-        Oblíbené:{" "}
-        <input
-          type="checkbox"
-          checked={isFavourite}
-          onChange={props.updateRecipeFavourite}
-        />
-      </h2>
-      <Link to={`/recipelist/${id}`}>
-        <Button>Detail</Button>
-      </Link>
-      <Button onClick={props.deleteRecipe} variant="danger">
-        Smazat
-      </Button>
-    </div>
+    <Card className="text-center" bg={isFavourite ? "warning" : null}>
+      <Card.Img src={image} />
+      <Card.Header>
+        <Card.Title>{title}</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>Název knihy: {book}</Card.Text>
+        <Card.Text>Strana: {page}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Text>Typ: {mealType}</Card.Text>
+        <Card.Text>Počet vaření: {cookedNumber}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Text>Náročnost: {difficulty}</Card.Text>
+        <Card.Text>Doba přípravy: {prepareTime} minut</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Text>
+          Oblíbené:{" "}
+          <input
+            type="checkbox"
+            checked={isFavourite}
+            onChange={props.updateRecipeFavourite}
+          />
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <div className="cardFooter-buttons">
+          <Link to={`/recipelist/${id}`}>
+            <Button>Detail</Button>
+          </Link>
+          <Button onClick={props.deleteRecipe} variant="danger">
+            Smazat
+          </Button>
+        </div>
+      </Card.Footer>
+    </Card>
   );
 };
 
