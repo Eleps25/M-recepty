fa0088701a1fd31d5fca248787ba368cfeafaec2
Jakub Mezulanik <mezuja@gmail.com>
2023-11-19 16:58:00 +0100

    firebase-update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5KF9wig31JkxjXn0x-Wj9tHoJF4JoWyw",
@@ -7,9 +8,10 @@ const firebaseConfig = {
   projectId: "m-recepty",
   storageBucket: "m-recepty.appspot.com",
   messagingSenderId: "351383567286",
-  appId: "1:351383567286:web:881e678a386bbc3effdf17"
+  appId: "1:351383567286:web:881e678a386bbc3effdf17",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
