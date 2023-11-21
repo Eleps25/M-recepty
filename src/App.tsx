fcd126059c9e465565fe2ecfebb419f029a67c72
Jakub Mezulanik <mezuja@gmail.com>
2023-11-21 20:05:24 +0100

    footer styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 /* import "./App.css"; */
 import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import RecipeDetailPage from "./components/RecipeDetailPage/RecipeDetailPage";
 import RecipeList from "./components/RecipeList/RecipeList";
 
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipelist" element={<RecipeList />} />
         <Route path="/recipelist/:id" element={<RecipeDetailPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
+import "./style.css";
+
 const Footer: React.FC = () => {
-  return <footer>Footer</footer>;
+  return <footer className="footer-container">
+    <div className="footer-copyright">©2023</div>
+    <a href="https://github.com/Eleps25" className="footer-created-by">Created by <span className="footer-created-by-myName">Jakub Mezuláník</span></a>
+  </footer>;
 };
 
 export default Footer;
